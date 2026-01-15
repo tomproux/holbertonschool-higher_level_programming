8f32cecef6356846429ca39e3d1be836a1a9f59a
tomproux <tomproux53@gmail.com>
2026-01-15 13:50:55 +0100

    ok

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+    from calculator_1 import calculation
+
+    a = 10
+    b = 5
+    print("{} + {} = {}".format(a, b, calculation(a, b)))
+    print("{} - {} = {}".format(a, b, calculation(a, b)))
+    print("{} * {} = {}".format(a, b, calculation(a, b)))
+    print("{} / {} = {}".format(a, b, calculation(a, b)))
