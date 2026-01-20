df13d4e7062e9ae1d2c64ae73b39cbaf1ef96fe9
tomproux <tomproux53@gmail.com>
2026-01-20 10:39:54 +0100

    ok

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,13 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    return matrix**2
-
-
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
-resultat = map(square_matrix_simple, matrix)
-print(resultat)
-print(list(resultat))
+    return list(map(lambda row: list(map(lambda num: num ** 2, row)), matrix))
