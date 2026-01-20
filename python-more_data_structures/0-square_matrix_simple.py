ff3ce4d00632c69eaf494b7090c7217ffce501fd
tomproux <tomproux53@gmail.com>
2026-01-20 10:24:33 +0100

    ok

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -3,7 +3,11 @@ def square_matrix_simple(matrix=[]):
     return matrix**2
 
 
-matrix = [1, 2, 3]
+matrix = [
+    [1, 2, 3]
+    [4, 5, 6]
+    [7, 8, 9]
+]
 resultat = map(square_matrix_simple, matrix)
 print(resultat)
 print(list(resultat))
