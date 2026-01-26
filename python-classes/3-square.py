3706e85a205698c281f9067ca60f847b1bebebef
tomproux <tomproux53@gmail.com>
2026-01-26 10:12:03 +0100

    ok

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 class Square:
 
     def __init__(self, size=0):
@@ -6,3 +7,6 @@ class Square:
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
+    def area(self):
+        return (self.__size * self.__size)
