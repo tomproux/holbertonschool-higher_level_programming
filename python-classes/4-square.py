9ff850b072fdbd66a1a1036049601ad9efc6128e
tomproux <tomproux53@gmail.com>
2026-01-26 10:30:47 +0100

    ok

---
+++ python-classes/4-square.py
@@ -0,0 +1,14 @@
+class Square:
+
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        return (self.__size * self.__size)
+
+    def print(self, area):
+        print("Area: {} for size: {}".format(area(self.__size), self.__size))
