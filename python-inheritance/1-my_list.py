ce28d97fe65b58e8aae6b19f40e5d5fdab38498b
tomproux <tomproux53@gmail.com>
2026-02-04 11:23:31 +0100

    ok

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -5,12 +5,8 @@ contains the MyList class
 
 
 class MyList(list):
-    """a subclass of list"""
-
-    def __init__(self):
-        """initializes the object"""
-        super().__init__()
+    """Implements sorted printing for the built-in list class."""
 
     def print_sorted(self):
-        """prints the sorted list"""
+        """Print a list in sorted ascending order."""
         print(sorted(self))
