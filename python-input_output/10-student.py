b4615e3ab6c00fa47bb4fb1f8aa80200c7e6b156
tomproux <tomproux53@gmail.com>
2026-02-11 10:00:04 +0100

    ok

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -1,32 +1,49 @@
 #!/usr/bin/python3
-"""Defines a class Student."""
+"""0x0B. Python - Input/Output, task 12. Student to JSON with filter"""
 
 
 class Student:
-    """Represent a student."""
+    """Simple class containing student data.
+
+    Args:
+        first_name (str): given name of student
+        last_name (str): family name of student
+        age (int): age of student in years
+
+    Attributes:
+        first_name (str): given name of student
+        last_name (str): family name of student
+        age (int): age of student in years
+
+    """
 
     def __init__(self, first_name, last_name, age):
-        """Initialize a new Student.
-
-        Args:
-            first_name (str): The first name of the student.
-            last_name (str): The last name of the student.
-            age (int): The age of the student.
-        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """Get a dictionary representation of the Student.
-
-        If attrs is a list of strings, represents only those attributes
-        included in the list.
+        """Retrieves a dictionary representation of self, minus any elements
+        whose keys are are not listed in `attrs`. If `attrs` is not a list of
+        strings, returns entire dict of self.
 
         Args:
-            attrs (list): (Optional) The attributes to represent.
+            attrs (list) of (str): list of keys for items in dictionary
+
+        Returns:
+            `ret_dict`, a dictionary for all of the keys listed in `attrs`,
+        or self__dict__ if there are any type conflicts for `attrs`.
+
         """
-        if (type(attrs) == list and
-                all(type(ele) == str for ele in attrs)):
-            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
-        return self.__dict__
+
+        ret_dict = {}
+        if type(attrs) is list:
+            for item in attrs:
+                if type(item) is not str:
+                    return self.__dict__
+            for key in attrs:
+                if key in self.__dict__:
+                    ret_dict.update({key: self.__dict__[key]})
+            return ret_dict
+        else:
+            return self.__dict__
