14ce0f786719aacd9a3508a96c6bd9dad1c7db12
tomproux <tomproux53@gmail.com>
2026-03-06 12:52:31 +0100

    ok

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,24 +1,28 @@
 #!/usr/bin/python3
 """
-deletes all State objects with a name containing the letter a from
-the database hbtn_0e_6_usa
+    Script that prints all City objects from the database.
 """
-
-from sys import argv
-from model_state import Base, State
-import sqlalchemy
-from sqlalchemy import create_engine
+import sys
+from sqlalchemy import (create_engine)
 from sqlalchemy.orm import Session
+
+from model_state import Base, State
 from model_city import City
 
-if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        argv[1], argv[2], argv[3]), pool_pre_ping=True)
 
+if __name__ == "__main__":
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(
+            sys.argv[1],
+            sys.argv[2],
+            sys.argv[3]
+        ),
+        pool_pre_ping=True
+    )
     Base.metadata.create_all(engine)
+
     session = Session(engine)
-    locations = session.query(City, State).filter(
-        City.state_id == State.id).order_by(City.id).all()
-    for city, state in locations:
-        print("{}: ({}) {}".format(state.name, city.id, city.name))
-    session.close()
+    cities = session.query(City, State).join(State).order_by(City.id).all()
+
+    for city, state in cities:
+        print(f"{state.name}: ({city.id}) {city.name}")
