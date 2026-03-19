709df5fccd5b3be0df0e6611124f0e854707001f
tomproux <tomproux53@gmail.com>
2026-03-19 14:44:10 +0100

    ok

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const array = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-for (let i = 0; i < array.length; i++) {
-  console.log(array[i]);
+const variable = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+for (const phrase in variable) {
+  console.log(variable[phrase]);
 }
