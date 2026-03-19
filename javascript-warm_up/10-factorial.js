2075f6c38017699a03c06c957ea3a988b03c6933
tomproux <tomproux53@gmail.com>
2026-03-19 14:39:33 +0100

    ok

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,6 +1,12 @@
 #!/usr/bin/node
 function factorial(n) {
-  return n === 0 || isNaN(n) ? 1 : n * factorial(n - 1);
+  if (n > 0) {
+    return (n * factorial(n - 1));
+  } else if (n === 0) {
+    return (1);
+  } else {
+    return (1);
+  }
 }
-
-console.log(factorial(Number(process.argv[2])));
+const number = Number(process.argv[2]);
+console.log(factorial(number));
