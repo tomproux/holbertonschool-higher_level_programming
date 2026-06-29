f5c7ce4ef533f1f3cadaad8812226b5b73dfb6eb
tomproux <tomproux53@gmail.com>
2026-06-29 15:54:59 +0200

    ok

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -2,17 +2,14 @@
 
 const args = process.argv.slice(2);
 
-if (args.length === 0)
-{
-    console.log('No argument');
+if (args.length === 0) {
+  console.log('No argument');
 }
 
-else if (args.length === 1) 
-{
-    console.log('Argument found');
+if (args.length === 1) {
+  console.log('Argument found');
 }
 
-else if (args.length > 1) 
-{
-    console.log('Arguments found');
+if (args.length > 1) {
+  console.log('Arguments found');
 }
