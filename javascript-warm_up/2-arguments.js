828717015835b429736c46c1c8da4f5aeed2bbef
tomproux <tomproux53@gmail.com>
2026-06-29 15:57:04 +0200

    ok

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -6,10 +6,10 @@ if (args.length === 0) {
   console.log('No argument');
 }
 
-if (args.length === 1) {
+else if (args.length === 1) {
   console.log('Argument found');
 }
 
-if (args.length > 1) {
+else if (args.length > 1) {
   console.log('Arguments found');
 }
