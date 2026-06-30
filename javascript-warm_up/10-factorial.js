dd6177157ff8aef6ce2a532a9f9648680c45a6fd
tomproux <tomproux53@gmail.com>
2026-06-30 10:56:46 +0200

    ok

---
+++ javascript-warm_up/10-factorial.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+function factorial(n) {
+  if (Number.isNaN(n) || n <= 1) {
+    return 1;
+  }
+
+  return n * factorial(n - 1);
+}
+
+const n = parseInt(process.argv[2]);
+
+console.log(factorial(n));
