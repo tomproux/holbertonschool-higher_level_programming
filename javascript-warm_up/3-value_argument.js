d32658d1c6c920e85bd2b0c0b531adbd3bc959b3
tomproux <tomproux53@gmail.com>
2026-06-30 09:45:29 +0200

    ok

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -3,7 +3,7 @@
 const [, , firstArg] = process.argv;
 
 if (firstArg === undefined) {
-    console.log('No argument');
+  console.log('No argument');
 } else {
-    console.log(firstArg);
+  console.log(firstArg);
 }
