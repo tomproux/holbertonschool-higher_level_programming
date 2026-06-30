89928a4ad2a50f58050abf695b24ae1aca1f1752
tomproux <tomproux53@gmail.com>
2026-06-30 10:43:36 +0200

    ok

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,2 +1,11 @@
 #!/usr/bin/node
 
+const x = Number.parseInt(process.argv[2]);
+
+if (Number.isNaN(x)) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < x; i++) {
+    console.log('C is fun');
+  }
+}
