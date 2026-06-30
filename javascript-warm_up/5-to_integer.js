ab9bc86a5087bd0342e9c9c9b3ad0e5ca6b78a03
tomproux <tomproux53@gmail.com>
2026-06-30 10:00:04 +0200

    ok

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -3,7 +3,7 @@
 const num = Number.parseInt(process.argv[2]);
 
 if (Number.isNaN(num)) {
-  console.log("Not a number");
+  console.log('Not a number');
 } else {
   console.log(`My number: ${num}`);
 }
