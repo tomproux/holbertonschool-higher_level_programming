e8ce2d7a13fbb6dc228fc0f95d830ab6d958de42
tomproux <tomproux53@gmail.com>
2026-06-30 10:44:37 +0200

    ok

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -1,2 +1,11 @@
 #!/usr/bin/node
 
+const size = Number.parseInt(process.argv[2]);
+
+if (Number.isNaN(size)) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < size; i++) {
+    console.log('X'.repeat(size));
+  }
+}
